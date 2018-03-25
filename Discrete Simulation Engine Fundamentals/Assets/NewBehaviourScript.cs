722c9b9434919979b7045b93e688850459562d6a
JCMatrix <2474223242@qq.com>
2018-03-25 11:24:53 +0800

    readme

---
+++ Discrete Simulation Engine Fundamentals/Assets/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    //public Transform prefab;
+
+    private void Start()
+    {
+       // Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+        gameObject.BroadcastMessage("ApplyDamage", 5.0F);
+    }
+    void ApplyDamage(float damage)
+    {
+        print(damage);
+    }
+}
---
+++ Discrete Simulation Engine Fundamentals/Assets/NewBehaviourScript1.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour {
+    void ApplyDamage(float damage)
+    {
+        print(damage);
+    }
+}
