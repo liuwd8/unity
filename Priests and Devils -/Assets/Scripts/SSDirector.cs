9c49870709e1538fdd6e19c4b9f577f019196e9c
JCMatrix <2474223242@qq.com>
2018-04-08 23:21:54 +0800

    11

---
+++ Priests and Devils -/Assets/Scripts/SSDirector.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SSDirector : System.Object {
+
+    private static SSDirector _instance;
+    public ISceneController currentSceneController { get; set; }
+    public bool running { get; set; }
+    private List<int> passedDevils;
+    private List<int> passedPriests;
+
+    SSDirector()
+    {
+        passedDevils = new List<int>();
+        passedPriests = new List<int>();
+    }
+
+    public static SSDirector getInstance()
+    {
+        if (_instance == null)
+            _instance = new SSDirector();
+        return _instance;
+    }
+
+    public int getFPS()
+    {
+        return Application.targetFrameRate;
+    }
+
+    public void setFPS(int fps)
+    {
+        Application.targetFrameRate = fps;
+    }
+    public void NextScene()
+    {
+
+    }
+}
