5d71d4d305c90cb6b2d4b80bcd66ff88d0a44fcb
JCMatrix <2474223242@qq.com>
2018-06-05 18:07:49 +0800

    UI
    Signed-off-by: JCMatrix <2474223242@qq.com>

---
+++ UI/Assets/script/IMGUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IMGUI : MonoBehaviour {
+    public float healthPanelOffset = 2f;
+    private void OnGUI()
+    {
+        Vector3 worldPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(worldPos);
+        Rect rect = new Rect(screenPos.x -50,screenPos.y,100,100);
+        GUI.HorizontalScrollbar(rect, 0, 75, 0, 100);
+    }
+}
---
+++ UI/Assets/script/UIScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour {
+    public Canvas canvas;
+    public GameObject healthPrefab;
+
+    public float healthPanelOffset = 2f;
+    public GameObject healthPanel;
+    private Slider healthSlider;
+
+    // Use this for initialization
+    void Start () {
+        healthPanel = Instantiate(healthPrefab) as GameObject;
+        healthPanel.transform.SetParent(canvas.transform, false);
+        healthSlider = healthPanel.GetComponentInChildren<Slider>();
+        Vector3 worldPos = new Vector3(transform.position.x, transform.position.y + healthPanelOffset, transform.position.z);
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(worldPos);
+        healthPanel.transform.position = new Vector3(screenPos.x, screenPos.y, screenPos.z);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 worldPos = new Vector3(transform.position.x, transform.position.y + healthPanelOffset, transform.position.z);
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(worldPos);
+        healthPanel.transform.position = new Vector3(screenPos.x, screenPos.y, screenPos.z);
+    }
+}
